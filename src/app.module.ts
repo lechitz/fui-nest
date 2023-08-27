31ceea46d878375a13aaa7056f7464c5b518be6b
Lechitz <felipe.lechitz@gmail.com>
2023-08-27 05:24:24 +0000

    Adjusting routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RoutesModule } from './routes/routes.module';
 
+@Global()
 @Module({
   imports: [PrismaModule, RoutesModule],
   controllers: [AppController],
